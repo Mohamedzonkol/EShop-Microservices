1402bcc3746971b3d5c7f343107c74d1ef04e890
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-06-25 20:08:55 +0300

    add create product command

--- Services/Catlog/CatlogApi/Products/CreateProduct/CreateProductEndPoint.cs
+++ Services/Catlog/CatlogApi/Products/CreateProduct/CreateProductEndPoint.cs
@@ -1,4 +1,21 @@
-﻿namespace CatlogApi.Products.CreateProduct
+﻿using BuildingBlocks.CQRS;
+using Carter;
+
+namespace CatlogApi.Products.CreateProduct
 {
-    public class CreateProductEndPoint { }
+    public record CreateProductRequest(
+        string Name,
+        List<string> Category,
+        string Description,
+        string ImageFile,
+        decimal Price) : ICommand<CreateProductResponse>;
+
+    public record CreateProductResponse(Guid Id);
+    public class CreateProductEndPoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
--- Services/Catlog/CatlogApi/Products/CreateProduct/CreateProductHandler.cs
+++ Services/Catlog/CatlogApi/Products/CreateProduct/CreateProductHandler.cs
@@ -1,20 +1,30 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using CatlogApi.Models;
 
 namespace CatlogApi.Products.CreateProduct
 {
-    public record CreateProductRequest(
+    public record CreateProductCommand(
         string Name,
         List<string> Category,
         string Description,
         string ImageFile,
-        decimal Price) : IRequest<CreateProductResponse>;
-    //Command
-    public record CreateProductResponse(Guid Id);//Result
-    internal class CreateProductHandler : IRequestHandler<CreateProductRequest, CreateProductResponse>
+        decimal Price) : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResponse> Handle(CreateProductRequest request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+            return new CreateProductResult(Guid.NewGuid());
         }
     }
 }
