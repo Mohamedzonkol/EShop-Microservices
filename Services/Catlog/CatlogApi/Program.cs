c0bcfb555fe8f8b39c41f193e72b9f5838d9647a
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-06-26 18:27:38 +0300

    Add marten Services

--- Services/Catlog/CatlogApi/Program.cs
+++ Services/Catlog/CatlogApi/Program.cs
@@ -1,13 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+//Add services to the container.
 builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
-
+builder.Services.AddMarten(opt =>
+{
+    opt.Connection(builder.Configuration.GetConnectionString("DefaultConnection")!);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
-
+//Pipeline
 app.MapCarter();
 app.Run();
