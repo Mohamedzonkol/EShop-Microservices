13d052f4243d77ceee79e2597c4bfa9927154e8f
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-06-28 19:25:56 +0300

    add custom generic exeption

---
+++ BuildingBlocks/BuildingBlocks/Exceptions/BadRequestException.cs
@@ -0,0 +1,14 @@
+﻿namespace BuildingBlocks.Exceptions
+{
+    public class BadRequestException : System.Exception
+    {
+        public BadRequestException(string message) : base(message)
+        {
+        }
+        public BadRequestException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) was not found.")
+        {
+        }
+
+    }
+}
---
+++ BuildingBlocks/BuildingBlocks/Exceptions/NotFoundException.cs
@@ -0,0 +1,14 @@
+﻿namespace BuildingBlocks.Exception
+{
+    public class NotFoundException : System.Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+        public NotFoundException(string message, string details) : base(message)
+        {
+            Details = details;
+        }
+        public string? Details { get; }
+    }
+}
