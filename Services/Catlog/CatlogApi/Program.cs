b33172285ede48d1cbad659e71ac2d42c6fa1786
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-06-28 19:12:34 +0300

    Add Globel Handling Exception

--- Services/Catlog/CatlogApi/Program.cs
+++ Services/Catlog/CatlogApi/Program.cs
@@ -1,4 +1,6 @@
 using BuildingBlocks.Behavouir;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 //Add services to the container.
@@ -19,4 +21,26 @@ var app = builder.Build();
 
 //Pipeline
 app.MapCarter();
+app.UseExceptionHandler(exceptionHandler =>
+{
+    exceptionHandler.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (exception == null)
+        {
+            return;
+        }
+        var problemDetails = new ProblemDetails()
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.StackTrace
+        };
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, exception.Message);
+        await context.Response.WriteAsJsonAsync(problemDetails);
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/json";
+    });
+});
 app.Run();
