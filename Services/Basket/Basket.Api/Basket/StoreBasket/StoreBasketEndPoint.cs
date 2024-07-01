4e1c7db2204e2c0a9526e973c1f434a7f7989456
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-01 15:51:37 +0300

    store end point

--- Services/Basket/Basket.Api/Basket/StoreBasket/StoreBasketEndPoint.cs
+++ Services/Basket/Basket.Api/Basket/StoreBasket/StoreBasketEndPoint.cs
@@ -1,6 +1,24 @@
-﻿namespace Basket.Api.Basket.StoreBasket
+﻿
+namespace Basket.Api.Basket.StoreBasket
 {
-    public class StoreBasketEndPoint
+    public record StoreBasketRequest(ShoppingCart Cart); /*: ICommand<StoreBasketResult>;*/
+    public record StoreBasketResponse(string userName);
+    public class StoreBasketEndPoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+                {
+                    var command = request.Adapt<StoreBasketCommand>();
+                    var result = await sender.Send(command);
+                    var response = result.Adapt<StoreBasketResponse>();
+                    return Results.Created($"/basket/{response.userName}", response);
+                }).WithName("CreateBasket")
+                .Produces<StoreBasketResponse>(201)
+                .ProducesProblem(400)
+                .WithSummary("Create Basket")
+                .WithDescription("Store Basket With Minimal Api");
+
+        }
     }
 }
--- Services/Basket/Basket.Api/Basket/StoreBasket/StoreBasketHandler.cs
+++ Services/Basket/Basket.Api/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,13 +1,13 @@
 ﻿namespace Basket.Api.Basket.StoreBasket
 {
     public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
-    public record StoreBasketResult(bool IsSuccess);
+    public record StoreBasketResult(string userName);
     public class StoreBasketHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
     {
         public async Task<StoreBasketResult> Handle(StoreBasketCommand request, CancellationToken cancellationToken)
         {
             ShoppingCart cart = request.Cart;
-            return new StoreBasketResult(true);
+            return new StoreBasketResult("true");
         }
     }
 
