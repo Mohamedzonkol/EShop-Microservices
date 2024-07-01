a7858dd949796a03db7642e38b84894adc031976
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-01 16:34:29 +0300

    create basket

--- Services/Basket/Basket.Api/Basket/StoreBasket/StoreBasketHandler.cs
+++ Services/Basket/Basket.Api/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,13 +1,15 @@
-﻿namespace Basket.Api.Basket.StoreBasket
+﻿using Basket.Api.Data;
+
+namespace Basket.Api.Basket.StoreBasket
 {
     public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
     public record StoreBasketResult(string userName);
-    public class StoreBasketHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    public class StoreBasketHandler(IBasketRepository basketRepository) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
     {
         public async Task<StoreBasketResult> Handle(StoreBasketCommand request, CancellationToken cancellationToken)
         {
-            ShoppingCart cart = request.Cart;
-            return new StoreBasketResult("true");
+            await basketRepository.StoreBasket(request.Cart, cancellationToken);
+            return new StoreBasketResult(request.Cart.UserName);
         }
     }
 
