d901dfe6ca1dc7f416f7ec514a59222c9fa56772
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-01 16:41:45 +0300

    add configrtion

--- Services/Basket/Basket.Api/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ Services/Basket/Basket.Api/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,11 +1,14 @@
-﻿namespace Basket.Api.Basket.DeleteBasket
+﻿using Basket.Api.Data;
+
+namespace Basket.Api.Basket.DeleteBasket
 {
     public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
     public record DeleteBasketResult(bool IsSuccess);
-    public class DeleteBasketHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+    public class DeleteBasketHandler(IBasketRepository basketRepository) : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
     {
         public async Task<DeleteBasketResult> Handle(DeleteBasketCommand request, CancellationToken cancellationToken)
         {
+            await basketRepository.DeleteBasket(request.UserName, cancellationToken);
             return new DeleteBasketResult(true);
         }
     }
--- Services/Basket/Basket.Api/Program.cs
+++ Services/Basket/Basket.Api/Program.cs
@@ -1,5 +1,9 @@
+using Basket.Api.Data;
 using BuildingBlocks.Behavior;
 using BuildingBlocks.Behavouir;
+using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 //Add services to the container.
@@ -12,8 +16,26 @@ builder.Services.AddMediatR(config =>
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
 
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+//builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+//  .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 var app = builder.Build();
 //pipeline
 app.MapCarter();
+app.UseExceptionHandler(options => { });
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
