176a4a86210c2fdecc5ef474affaff7fb2db7127
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-01 15:44:27 +0300

    add validtors

--- Services/Basket/Basket.Api/Basket/StoreBasket/StoreBasketHandler.cs
+++ Services/Basket/Basket.Api/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,14 @@
 ﻿namespace Basket.Api.Basket.StoreBasket
 {
-    public class StoreBasketHandler
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(bool IsSuccess);
+    public class StoreBasketHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
     {
+        public async Task<StoreBasketResult> Handle(StoreBasketCommand request, CancellationToken cancellationToken)
+        {
+            ShoppingCart cart = request.Cart;
+            return new StoreBasketResult(true);
+        }
     }
+
 }
---
+++ Services/Basket/Basket.Api/Basket/StoreBasket/StoreBasketValidtors.cs
@@ -0,0 +1,12 @@
+﻿namespace Basket.Api.Basket.StoreBasket
+{
+    public class StoreBasketValidtors : AbstractValidator<StoreBasketCommand>
+    {
+        public StoreBasketValidtors()
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Cart cannot be null");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("Username cannot be empty");
+            RuleFor(x => x.Cart.Items).Must(x => x.Count > 0).WithMessage("Cart should have atleast one item");
+        }
+    }
+}
