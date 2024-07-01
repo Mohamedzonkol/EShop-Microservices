d7f27ca81d603d2ab96f4dca2ea78c0dfab69832
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-01 15:25:31 +0300

    add domin modell

--- Services/Basket/Basket.Api/Models/ShoppingCart.cs
+++ Services/Basket/Basket.Api/Models/ShoppingCart.cs
@@ -2,5 +2,16 @@
 {
     public class ShoppingCart
     {
+        public string UserName { get; set; } = string.Empty;
+        public List<ShoppingCartItem> Items { get; set; } = new();
+        public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public ShoppingCart()
+        {
+        }
     }
 }
--- Services/Basket/Basket.Api/Models/ShoppingCartItem.cs
+++ Services/Basket/Basket.Api/Models/ShoppingCartItem.cs
@@ -2,5 +2,10 @@
 {
     public class ShoppingCartItem
     {
+        public int Quantity { get; set; } = default;
+        public string Color { get; set; } = string.Empty;
+        public decimal Price { get; set; } = default;
+        public Guid ProductId { get; set; } = default;
+        public string ProductName { get; set; } = string.Empty;
     }
 }
