929a39aaa5dfd24bd041a0f780276bb42b68790c
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-02 21:51:29 +0300

    add models

--- Services/Discount/Discount.Grpc/Models/Coupon.cs
+++ Services/Discount/Discount.Grpc/Models/Coupon.cs
@@ -2,5 +2,9 @@
 {
     public class Coupon
     {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Amount { get; set; }
     }
 }
