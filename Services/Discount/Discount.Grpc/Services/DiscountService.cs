314464cda0a1a0a3417c713ff2920671228b7f9d
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-03 17:15:39 +0300

    add mapster and first service

--- Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,17 +1,31 @@
-﻿using Discount.Grpc.Protos;
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
+using Discount.Grpc.Protos;
 using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
 
 namespace Discount.Grpc.Services
 {
-    public class DiscountService : Protos.DiscountService.DiscountServiceBase
+    public class DiscountService(DiscountContext dbContext, ILogger<DiscountService> logger)
+        : Protos.DiscountService.DiscountServiceBase
     {
-        public override Task<CouponResponse> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        public override async Task<CouponResponse> GetDiscount(GetDiscountRequest request, ServerCallContext context)
         {
-            return base.GetDiscount(request, context);
+            var coupon = await dbContext
+                .Coupons
+                .FirstOrDefaultAsync(x => x.ProductName == request.ProductName) ??
+                         new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+
+            logger.LogInformation("Discount is retrieved for ProductName : {productName}, Amount : {amount}", coupon.ProductName, coupon.Amount);
+
+            var couponResponse = coupon.Adapt<CouponResponse>();
+            return couponResponse;
         }
 
         public override Task<CouponResponse> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
         {
+
             return base.CreateDiscount(request, context);
         }
 
