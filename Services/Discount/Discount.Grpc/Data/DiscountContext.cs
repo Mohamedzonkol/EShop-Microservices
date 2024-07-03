91abe0b0555e78a58368b7d42b459e1707967adb
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-03 16:14:10 +0300

    add sqllite db

--- Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -1,6 +1,10 @@
-﻿namespace Discount.Grpc.Data
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
 {
-    public class DiscountContext
+    public class DiscountContext(DbContextOptions<DiscountContext> options) : DbContext(options)
     {
+        public DbSet<Coupon> Coupons { get; set; } = default!;
     }
 }
