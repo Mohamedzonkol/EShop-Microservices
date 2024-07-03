c4136104559f0b4741619d27e9b38d92a2c8d052
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-03 15:49:15 +0300

    add services

--- Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,6 +1,28 @@
-﻿namespace Discount.Grpc.Services
+﻿using Discount.Grpc.Protos;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services
 {
-    public class DiscountService
+    public class DiscountService : Protos.DiscountService.DiscountServiceBase
     {
+        public override Task<CouponResponse> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            return base.GetDiscount(request, context);
+        }
+
+        public override Task<CouponResponse> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            return base.CreateDiscount(request, context);
+        }
+
+        public override Task<CouponResponse> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            return base.UpdateDiscount(request, context);
+        }
+
+        public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            return base.DeleteDiscount(request, context);
+        }
     }
 }
