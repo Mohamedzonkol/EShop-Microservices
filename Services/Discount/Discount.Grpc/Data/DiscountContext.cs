4e97d33eec605bf8359f71fea0768d36e4c3f154
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-03 16:22:34 +0300

    seed data

--- Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -6,5 +6,16 @@ namespace Discount.Grpc.Data
     public class DiscountContext(DbContextOptions<DiscountContext> options) : DbContext(options)
     {
         public DbSet<Coupon> Coupons { get; set; } = default!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount", Amount = 150 },
+                new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 },
+                new Coupon { Id = 3, ProductName = "Huawei P30", Description = "Huawei Discount", Amount = 50 },
+                new Coupon { Id = 4, ProductName = "OnePlus 2", Description = "OnePlus Discount", Amount = 80 },
+                new Coupon { Id = 5, ProductName = "Xiaomi Mi 10", Description = "Xiaomi Discount", Amount = 200 }
+            );
+        }
     }
 }
