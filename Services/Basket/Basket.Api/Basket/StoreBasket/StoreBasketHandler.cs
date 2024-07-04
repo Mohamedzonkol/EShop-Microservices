55c872ad0ee8497c5afdf438ecf7086d0ca5c037
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-04 16:05:15 +0300

    connect with grpc services

--- Services/Basket/Basket.Api/Basket/StoreBasket/StoreBasketHandler.cs
+++ Services/Basket/Basket.Api/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,13 +1,21 @@
 ﻿using Basket.Api.Data;
+using Discount.Grpc.Protos;
 
 namespace Basket.Api.Basket.StoreBasket
 {
     public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
     public record StoreBasketResult(string userName);
-    public class StoreBasketHandler(IBasketRepository basketRepository) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    public class StoreBasketHandler(IBasketRepository basketRepository, DiscountService.DiscountServiceClient discountGrpc) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
     {
         public async Task<StoreBasketResult> Handle(StoreBasketCommand request, CancellationToken cancellationToken)
         {
+            //communicate with Discount Grpc Services
+            foreach (var item in request.Cart.Items)
+            {
+                var coupon = await discountGrpc.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName });
+                item.Price -= coupon.Amount;
+
+            }
             await basketRepository.StoreBasket(request.Cart, cancellationToken);
             return new StoreBasketResult(request.Cart.UserName);
         }
