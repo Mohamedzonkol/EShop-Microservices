a4efc8f3ab3f7809bfc7164cf51201746b5c9b00
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-07 16:50:06 +0300

    add entity

---
+++ Services/Ordering/Ordering.Domin/Abstractions/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace Ordering.Domin.Abstractions
+{
+    public abstract class Entity<T> : IEntity<T>
+    {
+        public T Id { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string? UpdatedBy { get; set; }
+
+    }
+}
---
+++ Services/Ordering/Ordering.Domin/Abstractions/IEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Ordering.Domin.Abstractions
+{
+    public interface IEntity<T> : IEntity
+    {
+        public T Id { get; set; }
+    }
+    public interface IEntity
+    {
+        public DateTime? CreatedAt { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string? UpdatedBy { get; set; }
+    }
+}
