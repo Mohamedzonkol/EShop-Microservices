312875035d6d2a551ed8b0d8035ca8395581789f
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-09 15:55:46 +0300

    update value object

--- Services/Ordering/Ordering.Domin/ValueObject/Payment.cs
+++ Services/Ordering/Ordering.Domin/ValueObject/Payment.cs
@@ -4,9 +4,30 @@
     {
         public string CardNumber { get; } = default!;
         public string CardHolderName { get; } = default!;
-        public DateTime Expiration { get; } = default!;
+        public string Expiration { get; } = default!;
         public string CVV { get; } = default!;
         public int PaymentMethod { get; } = default!;
+        protected Payment()
+        {
+        }
 
+        private Payment(string cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
+        {
+            CardHolderName = cardName;
+            CardNumber = cardNumber;
+            Expiration = expiration;
+            CVV = cvv;
+            PaymentMethod = paymentMethod;
+        }
+
+        public static Payment Of(string cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(cardName);
+            ArgumentException.ThrowIfNullOrWhiteSpace(cardNumber);
+            ArgumentException.ThrowIfNullOrWhiteSpace(cvv);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(cvv.Length, 3);
+
+            return new Payment(cardName, cardNumber, expiration, cvv, paymentMethod);
+        }
     }
 }
--- Services/Ordering/Ordering.Infrastructure/Data/Extenstions/DatabaseExtenstions.cs
+++ Services/Ordering/Ordering.Infrastructure/Data/Extenstions/DatabaseExtenstions.cs
@@ -9,7 +9,39 @@ namespace Ordering.Infrastructure.Data.Extenstions
             using var scope = app.Services.CreateScope();
             var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
             await context.Database.MigrateAsync();
+            await SeedAsync(context);
+        }
 
+        private static async Task SeedAsync(AppDbContext context)
+        {
+            await seedCustomerAsync(context);
+            await SeedOrdersWithItemsAsync(context);
+            await SeedProductAsync(context);
+
+        }
+        private static async Task seedCustomerAsync(AppDbContext context)
+        {
+            if (!await context.Customers.AnyAsync())
+            {
+                await context.Customers.AddRangeAsync(InitialData.Customers);
+                await context.SaveChangesAsync();
+            }
+        }
+        private static async Task SeedProductAsync(AppDbContext context)
+        {
+            if (!await context.Products.AnyAsync())
+            {
+                await context.Products.AddRangeAsync(InitialData.Products);
+                await context.SaveChangesAsync();
+            }
+        }
+        private static async Task SeedOrdersWithItemsAsync(AppDbContext context)
+        {
+            if (!await context.Orders.AnyAsync())
+            {
+                await context.Orders.AddRangeAsync(InitialData.OrdersWithItems);
+                await context.SaveChangesAsync();
+            }
         }
     }
 }
