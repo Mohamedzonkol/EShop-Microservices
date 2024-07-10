ee389aef0731fca969c788ea8d2a5d14fd9d4ebd
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-10 17:41:01 +0300

    add order Extentions

--- Services/Ordering/Ordering.Applecation/globalUsing.cs
+++ Services/Ordering/Ordering.Applecation/globalUsing.cs
@@ -6,6 +6,7 @@ global using Microsoft.Extensions.DependencyInjection;
 global using Ordering.Applecation.Data;
 global using Ordering.Applecation.Dtos;
 global using Ordering.Applecation.Exceptions;
+global using Ordering.Applecation.Extentions;
 global using Ordering.Domin.Enums;
 global using Ordering.Domin.Models;
 global using Ordering.Domin.ValueObject;
