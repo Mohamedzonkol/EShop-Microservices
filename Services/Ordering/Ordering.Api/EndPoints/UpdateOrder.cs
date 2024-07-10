40df1d09812a690c7da2a217fae47a2bdcb453a2
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-10 20:50:14 +0300

    update order End point

--- Services/Ordering/Ordering.Api/EndPoints/UpdateOrder.cs
+++ Services/Ordering/Ordering.Api/EndPoints/UpdateOrder.cs
@@ -1,6 +1,29 @@
-﻿namespace Ordering.Api.EndPoints
+﻿using Carter;
+using Mapster;
+using MediatR;
+using Ordering.Applecation.Dtos;
+using Ordering.Applecation.Orders.Commands.UpdateOrder;
+
+namespace Ordering.Api.EndPoints
 {
-    public class UpdateOrder
+    public record UpdateOrderRequest(OrderDto Order);
+    public record UpdateOrderResponse(bool IsSuccess);
+
+    public class UpdateOrder : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/orders", async (UpdateOrderResponse req, ISender sender) =>
+                {
+                    var command = req.Adapt<UpdateOrderCommand>();
+                    var res = await sender.Send(command);
+                    var response = res.Adapt<UpdateOrderResponse>();
+                    return Results.Ok(response);
+                }).WithName("UpdateOrder")
+                .Produces<UpdateOrderResponse>(200)
+                .ProducesProblem(400)
+                .WithSummary("Update Order")
+                .WithDescription("Update Order");
+        }
     }
 }
