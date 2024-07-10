0b734d9dab6dc26b37fef73289b36ccc3f562c6b
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-10 17:00:13 +0300

    add iapplication dbcontext

--- Services/Ordering/Ordering.Applecation/Data/IAppDbContext.cs
+++ Services/Ordering/Ordering.Applecation/Data/IAppDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace Ordering.Applecation.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domin.Models;
+
+namespace Ordering.Applecation.Data
 {
     public interface IAppDbContext
     {
+        DbSet<Customer> Customers { get; }
+        DbSet<Product> Products { get; }
+        DbSet<Order> Orders { get; }
+        DbSet<OrderItem> OrderItems { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
 }
