d8c3625ed2a570e2715abb1bdd2b175724664566
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-10 21:04:52 +0300

    get order by name or custumer name end point

--- Services/Ordering/Ordering.Api/EndPoints/GetOrderByCustomerName.cs
+++ Services/Ordering/Ordering.Api/EndPoints/GetOrderByCustomerName.cs
@@ -1,6 +1,26 @@
-﻿namespace Ordering.Api.EndPoints
+﻿using Ordering.Applecation.Orders.Queries.GetOrderByCustomer;
+
+namespace Ordering.Api.EndPoints
 {
-    public class GetOrderByCustomerName
+    public record GetOrdersByCustomerResponse(IEnumerable<OrderDto> Orders);
+
+    public class GetOrderByCustomerName : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders/customer/{customerId}", async (Guid customerId, ISender sender) =>
+                {
+                    var result = await sender.Send(new GetOrdersByCustomerQuery(customerId));
+
+                    var response = result.Adapt<GetOrdersByCustomerResponse>();
+
+                    return Results.Ok(response);
+                })
+                .WithName("GetOrdersByCustomer")
+                .Produces<GetOrdersByCustomerResponse>(200)
+                .ProducesProblem(404)
+                .WithSummary("Get Orders By Customer")
+                .WithDescription("Get Orders By Customer");
+        }
     }
 }
--- Services/Ordering/Ordering.Api/EndPoints/GetOrderByName.cs
+++ Services/Ordering/Ordering.Api/EndPoints/GetOrderByName.cs
@@ -1,6 +1,25 @@
-﻿namespace Ordering.Api.EndPoints
+﻿using Ordering.Applecation.Orders.Queries.GetOrderByName;
+namespace Ordering.Api.EndPoints
 {
-    public class GetOrderByName
+    public record GetOrdersByNameResponse(IEnumerable<OrderDto> Orders);
+
+    public class GetOrderByName : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders/{name}", async (string name, ISender sender) =>
+            {
+                var result = await sender.Send(new GetOrdersByNameQuery(name));
+
+                var response = result.Adapt<GetOrdersByNameResponse>();
+
+                return Results.Ok(response);
+            }).WithName("GetOrderByName")
+            .Produces<GetOrdersByNameResponse>(200)
+            .ProducesProblem(400)
+            .ProducesProblem(404)
+            .WithSummary("Get Order By Name")
+            .WithDescription("Get Order By Name");
+        }
     }
 }
