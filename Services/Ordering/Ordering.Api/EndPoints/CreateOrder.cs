04b712c2fdfb438c47a1fcbdde474610247bf487
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-10 20:47:28 +0300

    create order end point

--- Services/Ordering/Ordering.Api/EndPoints/CreateOrder.cs
+++ Services/Ordering/Ordering.Api/EndPoints/CreateOrder.cs
@@ -1,6 +1,29 @@
-﻿namespace Ordering.Api.EndPoints
+﻿using Carter;
+using Mapster;
+using MediatR;
+using Ordering.Applecation.Dtos;
+using Ordering.Applecation.Orders.Commands.CreateOrder;
+
+namespace Ordering.Api.EndPoints
 {
-    public class CreateOrder
+    public record CreateOrderRequest(OrderDto Order);
+    public record CreateOrderResponse(Guid Id);
+    public class CreateOrder : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/orders", async (CreateOrderRequest req, ISender sender) =>
+            {
+                var command = req.Adapt<CreateOrderCommand>();
+                var res = await sender.Send(command);
+                var response = res.Adapt<CreateOrderResponse>();
+                return Results.Created($"/orders/{response.Id}", response);
+
+            }).WithName("CreateOrder")
+            .Produces<CreateOrderResponse>(201)
+            .ProducesProblem(400)
+            .WithSummary("Create Order")
+            .WithDescription("Create Order");
+        }
     }
 }
