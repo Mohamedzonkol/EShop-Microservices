68630b74cd5c01af6ef61768245d3b3cfdab4dd5
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-10 17:46:41 +0300

    add Get Orders By Customer Handler

---
+++ Services/Ordering/Ordering.Applecation/Orders/Queries/GetOrderByCustomer/GetOrdersByCustomerHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace Ordering.Applecation.Orders.Queries.GetOrderByCustomer
+{
+    public class GetOrdersByCustomerHandler(IAppDbContext Context)
+        : IQueryHandler<GetOrdersByCustomerQuery, GetOrdersByCustomerResult>
+    {
+        public async Task<GetOrdersByCustomerResult> Handle(GetOrdersByCustomerQuery query, CancellationToken cancellationToken)
+        {
+            var orders = await Context.Orders
+                .Include(o => o.OrderItems)
+                .AsNoTracking()
+                .Where(o => o.CustomerId == CustomerId.Of(query.CustomerId))
+                .OrderBy(o => o.OrderName.Value)
+                .ToListAsync(cancellationToken);
+
+            return new GetOrdersByCustomerResult(orders.ToOrderDtoList());
+        }
+    }
+}
---
+++ Services/Ordering/Ordering.Applecation/Orders/Queries/GetOrderByCustomer/GetOrdersByCustomerQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace Ordering.Applecation.Orders.Queries.GetOrderByCustomer
+{
+    public record GetOrdersByCustomerQuery(Guid CustomerId)
+        : IQuery<GetOrdersByCustomerResult>;
+
+    public record GetOrdersByCustomerResult(IEnumerable<OrderDto> Orders);
+}
