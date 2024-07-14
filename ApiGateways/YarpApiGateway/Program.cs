c91a13a707d6f2f590d8a8bf7f0f1c939bbb6c2f
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-14 15:56:10 +0300

    add yarp reverse proxy services

--- ApiGateways/YarpApiGateway/Program.cs
+++ ApiGateways/YarpApiGateway/Program.cs
@@ -1,10 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 //Services
-
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 //pipeline
-
+app.MapReverseProxy();
 
 
 app.Run();
